5d7a02403552939ee1f0abb052d10b717a7517b7
Yihun Alemayehu <yankure01@gmail.com>
2024-02-01 22:26:43 +0300

    final

---
+++ Final/final.js
@@ -0,0 +1,9 @@
+var s1 = "javascript from javapoint index of ";
+var result = s1.lastIndexOf("index");
+
+var str= "javascript";
+
+var result = s1.concat(str);
+
+console.log(str.charAt(2));
+console.log(result);
